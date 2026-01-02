7c250390c8a957b3552b80525a36aca4b031e00e
msidiq2235 <muhsidiq2005@gmail.com>
2026-01-02 22:03:05 +0700

    membuat file DestinasiEntry.kt dan mengisi kodenya

---
+++ app/src/main/java/com/example/pt14firebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.pt14firebase.view.route
+
+import com.example.pt14firebase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
