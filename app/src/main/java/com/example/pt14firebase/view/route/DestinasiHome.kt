da95dd3c059ac5b1084adb0beb44ae624d1d3b64
msidiq2235 <muhsidiq2005@gmail.com>
2026-01-02 22:03:15 +0700

    membuat file DestinasiHome.kt dan mengisi kodenya

---
+++ app/src/main/java/com/example/pt14firebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pt14firebase.view.route
+
+import com.example.pt14firebase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
