eca1e76be3b486f2d9d4216eff16f8182c7231a3
msidiq2235 <muhsidiq2005@gmail.com>
2026-01-02 22:05:56 +0700

    mengubah MainActivity.kt agar bisa dirun

--- app/src/main/java/com/example/pt14firebase/MainActivity.kt
+++ app/src/main/java/com/example/pt14firebase/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.pt14firebase.ui.theme.Pt14firebaseTheme
+import com.example.pt14firebase.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pt14firebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pt14firebaseTheme {
-        Greeting("Android")
     }
 }
